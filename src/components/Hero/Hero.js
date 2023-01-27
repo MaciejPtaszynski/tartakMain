d15e642026ea3cc8c65bd87bdc547361f089b28d
Maciej Ptaszynski <mpsendit@gmail.com>
2023-01-27 23:20:49 +0100

    noQuery add footer and offer comps

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import './Hero.css'
 import ContactForm from '../ContactForm/ContactForm'
-import logoTartak from "../../library/logoTartak.png";
+
 
 function Hero() {
   return (
     <div className={"heroWrapper"}>
-      <div className={"logo"}>
-        <img className={"company-img"} alt="company logo" src={logoTartak}/>
-      </div>
+    
       <div className={"companyInfo"}>
         <h1>TARTAK<br/>Markowski Wojtowicz S.C.</h1>
         <h2>Drewno konstrukcyjne</h2>
         <h2>Deski tarasowe</h2>
-        <h2>Transport</h2>
-        <div className={"contact-form"}>
+        <h2>Transport</h2> 
+      </div>
+      <div className={"contact-form"}>
           <ContactForm/>
         </div>
-      </div>
     </div>
   )
 }
