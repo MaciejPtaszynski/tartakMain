eae9d524264c1c374a1e5aeebc7cc5d81768ad5d
Przemyslaw Rosiak <woot6091@gmail.com>
2023-01-28 00:49:11 +0100

    refactor mediaQuery, footer, add redirecting links to phoneIcon, localisationIcon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,25 +2,36 @@ import React from 'react'
 import "./Footer.css"
 import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-// import HomeIcon from '@mui/icons-material/Home';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
 import logoTartak from "../../library/logoTartak.png";
-import DescriptionIcon from '@mui/icons-material/Description';
+
+const sawmillUrl = "https://www.google.com/maps/place/Jantar+Le%C5%9Bnicz%C3%B3wka+20A,+82-103+Jantar+Le%C5%9Bnicz%C3%B3wka/@54.3186839,19.0035752,18z/data=!4m6!3m5!1s0x46fd6abd6a25cbf5:0xf4a594632551f5d7!8m2!3d54.3185417!4d19.0036826!16s%2Fg%2F11hyt2xxlm?hl=pl-PL";
 
 function Footer() {
+
+
   return (
     <div className='footerContainer'>
       <div className="footerLogo"><img alt="company logo" src={logoTartak}/></div>
       <div className="footerIcons">
-        <div className="footerIcon"><PhoneAndroidIcon className={"icon"} sx={{marginRight: "2px"}}/><p>534 916 564</p>
+        <div className="footerIcon col">
+          <h4>Dane do faktury:</h4>
+          <p className={"invoice-info"}>Tartak Markowski Wojtowicz S.C.<br/>
+            Regon: 12345678901234<br/>
+            NIP: 1234567890 <br/>
+            Kościuszki 49<br/>
+            80-112 Ostaszewo
+          </p>
         </div>
+        <a href="tel:534916564" className="footerIcon"><PhoneAndroidIcon style={{marginRight: "10px"}}
+                                                                         className={"icon"}/> 534 916 564
+        </a>
         <a className="footerIcon" href="mailto:jantartartak@gmail.com"><AlternateEmailIcon className={"icon"}/>
           <p>jantartartak@gmail.com</p></a>
-        <div className="footerIcon"><DescriptionIcon className={"icon"}/><p>Dane do Faktury</p></div>
-        <a href="#" className="footerIcon"><MyLocationIcon className={"icon"}/><p className="center">Jantar Leśniczówka
+        <a href={sawmillUrl} className="footerIcon"><MyLocationIcon className={"icon"}/><p className="center">Jantar
+          Leśniczówka
           20a<br/>82-103 Jantar</p></a>
       </div>
-
     </div>
   )
 }
